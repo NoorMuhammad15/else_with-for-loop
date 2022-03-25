3b533db80b65a9d4416c4a487c827910304ad965
Noor Muhammad <noorsarki15@gmail.com>
2022-03-25 08:27:52 +0500

    Add files via upload

---
+++ else_with_forloop.py
@@ -0,0 +1,12 @@
+khana=["roti","sabzi","chawal"]
+
+
+for item in khana:
+    # print(item)
+    if item =="parata":
+         break # only roti will print
+
+else:
+    # print("this for loop eneded properly")
+    print("item was not found")
+
